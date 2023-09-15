184b0875cf2ce66ccb8c409c8be5b3244f719414
ANNMAINAWANGARI <wangariann371@gmail.com>
2023-09-15 14:21:09 +0300

    fixed mongodb buffering issue

--- index.js
+++ index.js
@@ -8,20 +8,28 @@ require('dotenv').config();
 
 const DB_URL = process.env.DB_URL
 
-mongoose.connect(DB_URL);
-const database = mongoose.connection
+// mongoose.connect(DB_URL);
+// const database = mongoose.connection
 
 
-database.on('error', (error) => {
-    console.log('error is:',error)
-})
+// database.on('error', (error) => {
+//     console.log('error is:',error)
+// })
 
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+// database.once('connected', () => {
+//     console.log('Database Connected');
+// })
 app.use(express.json());
 app.use('/api',routes)
 
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 })
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Unable to connect to MongoDB:', error)
+  })
